23d96abfbdb81ecd2cd88adfc2a2a3e6b27b797a
Christopher Gallo <cjg100@georgetown.edu>
2020-12-02 00:02:21 -0500

    some js progress'

---
+++ html_css_js_together/app.js
@@ -0,0 +1,6 @@
+// quick javascript that can be run in html
+
+let userInput = prompt("test");
+
+console.log(userInput);
+
---
+++ html_css_js_together/quicknotes.js
@@ -0,0 +1,15 @@
+// just trying out some string methods
+
+let example = "woah".toUpperCase();
+
+
+let myName = "SamSmith";
+console.log(myName.indexOf('Smith'));
+
+
+
+// Equality
+
+// == vs ===
+// double equal checks equality by value but not by type
+// === checks for equality by value and type
