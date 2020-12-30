0220eeb9f1a441b0e47d7151b788d65e57b0d14f
cjglo <chris.j.gallo3@gmail.com>
2020-12-29 23:01:35 -0500

    finished up section, now onto basics

--- html_css_js_together/quicknotes.js
+++ html_css_js_together/quicknotes.js
@@ -13,3 +13,24 @@ console.log(myName.indexOf('Smith'));
 // == vs ===
 // double equal checks equality by value but not by type
 // === checks for equality by value and type
+
+
+// True and False
+
+// Big note here is that several values are evaluted to false:
+// false, 0, "", null, undefined, Nan
+// everything else is true
+
+
+console.log("");
+let num;
+
+if(num)
+{
+	console.log("its true!");
+}
+else
+{
+	console.log("its false"); // this is run
+}
+console.log("\n");
