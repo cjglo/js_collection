b8ac4b470a45a704853e34bd2ca881adb2921862
cjglo <chris.j.gallo3@gmail.com>
2021-01-28 20:16:29 -0500

    started events stub and moving to exception handling

---
+++ 102_intermed/event_driven.js
@@ -0,0 +1,11 @@
+
+//  *** event listeners ***
+// think like Java Swing library
+
+// creating a listener on button
+document.getElementById("lemons").onclick = () => console.log("click");
+
+// can set breakpoint inside "Sources" of web console, which lets pausing for events, since a lot of event driven code
+
+
+// Will be expanded later, but will be a while so this is just a stub
---
+++ debug.js
@@ -0,0 +1,12 @@
+// very quick note to make debugging esier:
+
+// can set break point inside web console and at that point, the values of varaibles will be displayed
+
+// there is a continue to move to next breakpont
+
+
+let total = 1;
+ for (let i = 3; i > 0; i--) {
+ 	total *= 2; // if break was here, will continue to next loop point
+ }
+
